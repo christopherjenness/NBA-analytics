269df493dfb4d9bc7c86ceb0c27fc674b6096919
Christopher Jenness <christopherjenness@gmail.com>
2016-12-22 08:11:44 -0500

    Organize code

--- AST/ast.py
+++ AST/ast.py
@@ -2,23 +2,6 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('AST.csv')
-df2015 = pd.read_csv('AST2015.csv')
-
-plt.scatter(df['PASSES MADE'], df['AST'])
-
-plt.scatter(df2015['AST'], df2015['SECONDARY AST'])
-plt.xlabel('Primary Assists')
-plt.ylabel('Secondary Assits')
-
-plt.scatter(df2015['AST'], df2015['FT AST'])
-
-plt.scatter(df['PASSES MADE'], df['PASSES RECEIVED'])
-
-plt.scatter(df2015['AST'], df2015['POTENTIAL AST'])
-
-df2015.sort_values('SECONDARY AST',ascending=False ).head()
-
 def set_plot_params(size):
     SIZE = size
     plt.rc('font', size=SIZE)  
@@ -26,21 +9,7 @@ def set_plot_params(size):
     plt.rc('axes', labelsize=SIZE)  
     plt.rc('xtick', labelsize=SIZE)  
     plt.rc('ytick', labelsize=SIZE)
-    plt.rc('legend', fontsize=SIZE)  
-
-set_plot_params(22)
-
-df2015 = df2015[df2015.AST > 100]
-sns.swarmplot(df2015['POTENTIAL AST']/df2015['AST'])
-
-df[df.AST  >30].sort_values('AST VAL', ascending=False).head(20)
-
-df['AST VAL'] = df['AST PTS'] / df['AST']
-plt.scatter(df['AST'], df['AST PTS'])
-df.sort_values('AST VAL', ascending=False).head(20)
-
-df2015['potentialper'] = df2015['POTENTIAL AST']/df2015['AST']
-df2015.sort_values('potentialper', ascending=False).head()
+    plt.rc('legend', fontsize=SIZE) 
 
 def threePTs(df):
     AST = df['AST']
@@ -51,9 +20,58 @@ def threePTs(df):
         ASTPT -= 3
         AST -= 1
     return counter
-    
-df['3AST'] = df.apply(threePTs, axis=1)
-df['2AST'] = df['AST'] - df['3AST']
 
-plt.scatter(df['2AST'], df['3AST'])
-df.sort_values('3AST', ascending=False).head()
+def organize_data():
+    df = pd.read_csv('AST.csv')
+    df['3AST'] = df.apply(threePTs, axis=1)
+    df['2AST'] = df['AST'] - df['3AST']
+    return df
+    
+def plot_secondary_assists(df):
+    plt.figure()
+    plt.scatter(df['AST'], df['SECONDARY AST'], s=60 ,alpha=0.6)
+    plt.xlabel('Primary Assists')
+    plt.ylabel('Secondary Assits')   
+    plt.title('Steph Curry is the king \n of secondary assists')
+    plt.xlim(0)
+    plt.ylim(0)
+    plt.text(190, 65, 'Curry')
+    plt.show()
+    
+def plot_team_secondary_assists(df):
+    plt.figure()
+    team_AST = df.groupby('TEAM').sum()
+    team_AST['SECONDARY AST'].sort_values(ascending=False).plot(kind='bar')
+    plt.title('Secondary Assists\n by Team')
+    plt.ylabel('Secondary Assists')
+    plt.show()
+    
+def plot_assist_value(df):
+    plt.figure()
+    plt.scatter(df['2AST'], df['3AST'], s=60, alpha=0.6)
+    plt.text(95, 132, 'LeBron')
+    plt.text(205, 145, 'Harden')
+    plt.xlabel('2PT Assists')
+    plt.ylabel('3PT Assists')
+    plt.title('Player Assist Value')
+    plt.xlim(0, 250)
+    plt.ylim(0, 160)
+    plt.show()
+    
+def plot_potential_assists(df):
+    plt.figure()
+    plt.scatter(df['AST'], df['POTENTIAL AST'], s=60, alpha=0.6)
+    plt.xlim(0)
+    plt.ylim(0)
+    plt.xlabel('Assists')
+    plt.ylabel('Potential Assists')
+    plt.title('A potential assist is \nworth half an assist')
+    plt.show()
+
+if __name__ == 'main':
+    set_plot_params(22)
+    df = organize_data()
+    plot_secondary_assists(df)
+    plot_team_secondary_assists(df)
+    plot_assist_value(df)  
+    plot_potential_assists(df)
