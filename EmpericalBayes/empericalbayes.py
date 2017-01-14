333b9e9ac8c2703f7b9e4469ea7df882813f69c7
Christopher Jenness <christopherjenness@gmail.com>
2017-01-14 08:42:30 -0500

    Organize into functions

--- EmpericalBayes/empericalbayes.py
+++ EmpericalBayes/empericalbayes.py
@@ -4,47 +4,74 @@ import matplotlib.pyplot as plt
 import matplotlib
 from scipy.stats import beta
 
-df = pd.read_csv('2016stats.csv')
-df = df[df['3PA'] > 20]
-df['3P%'] = df['3P%']/100
+def set_plot_params(size):
+    SIZE = size
+    plt.rc('font', size=SIZE)  
+    plt.rc('axes', titlesize=SIZE)  
+    plt.rc('axes', labelsize=SIZE)  
+    plt.rc('xtick', labelsize=SIZE)  
+    plt.rc('ytick', labelsize=SIZE)
+    plt.rc('legend', fontsize=SIZE)  
+    plt.rc('figure', titlesize=SIZE)
 
-plt.hist(df['3P%'], bins=30)
+def organize_data():
+    df = pd.read_csv('2016stats.csv')
+    df = df[df['3PA'] > 20]
+    df['3P%'] = df['3P%']/100
+    a = beta.fit(list(df['3P%']),floc=0, fscale=1)[0]
+    b =  beta.fit(list(df['3P%']),floc=0, fscale=1)[1]
+    df['3PEstimate'] = (df['3PM'] + a) / (df['3PA'] + a + b)
+    print('alpha: ' + str(a))
+    print('beta: ' + str(b))
+    return (df, a, b)
 
-a = beta.fit(list(df['3P%']),floc=0, fscale=1)[0]
-b =  beta.fit(list(df['3P%']),floc=0, fscale=1)[1]
+def make_plots(df, a, b):
+    plt.figure()
+    plt.hist(df['3P%'], bins=30)
+    plt.xlabel('3PT %')
+    plt.ylabel('Number of Players')
+    plt.title('Distribution of 3PT%')
+    plt.savefig('plots/3PT%.png')
+    
+    plt.figure()
+    x = np.linspace(0.01, 0.99, 100)
+    y = beta.pdf(x, a, b)
+    plt.hist(df['3P%'], bins=30, normed=True, label='Emperical')
+    plt.plot(x, y, 'k-', lw=2, label='Beta')
+    plt.xlabel('3PT %')
+    plt.ylabel('Number of Players')
+    plt.title('Beta Approximation of 3PT%')
+    plt.legend()
+    plt.xlim(0.1, 0.7)
+    plt.savefig('plots/betaapprox.png')
+
+    plt.figure()
+    plt.hist(df['3P%'], bins=30, normed=True, label='Emperical', alpha=0.6)
+    plt.hist(df['3PEstimate'], bins =12, alpha=0.6, label='Estimate', normed=True)
+    plt.xlabel('3PT %')
+    plt.ylabel('Number of Players')
+    plt.title('Bayesian Estimation of 3PT%')
+    plt.legend()
+    plt.savefig('plots/estimation.png')
+    
+    plt.figure()
+    y1 = beta.pdf(x, a, b)
+    y2 = beta.pdf(x, 58.54, 73.47)
+    plt.plot(x, y1, 'r-', lw=5, alpha=0.6, label='League\nDistribution')
+    plt.plot(x, y2, 'r-', lw=5, alpha=0.6, label='George\nHill')
+    plt.xlabel('3PT %')
+    plt.ylabel('3PT% Estimate')
+    plt.title('Paul George \nBayesian Estimation of 3PT%')
+    plt.legend()
+    plt.savefig('plots/paulgeorge.png')
 
 
-
-x = np.linspace(0.01, 0.99, 100)
-y = beta.pdf(x, a, b)
-ax.plot(x, beta.pdf(x, a, b), 'r-', lw=5, alpha=0.6, label='beta pdf')
-
-rv = beta(a, b)
-
-plt.hist(df['3P%'], bins=30, normed=True, label='Emperical')
-plt.plot(x, rv.pdf(x), 'k-', lw=2, label='Beta')
-plt.legend()
-
-df['3PEstimate'] = (df['3PM'] + a) / (df['3PA'] + a + b)
-
-plt.hist(df['3P%'], bins=30, normed=True, label='Emperical', alpha=0.6)
-plt.hist(df['3PEstimate'], bins =30, alpha=0.6, label='Estimate')
-plt.legend()
 
 df.sort_values('3PEstimate', ascending=False)[['PLAYER',  '3PEstimate', 'a', 'b']]
 
-df['a'] = df['3PM'] + a
-df['b'] = df['3PA'] - df['3PM'] + b
-
-x = np.linspace(0.01, 0.99, 100)
-y1 = beta.pdf(x, a, b)
-y2 = beta.pdf(x, 58.54, 73.47)
-plt.plot(x, y1, 'r-', lw=5, alpha=0.6, label='League\nDistribution')
-plt.plot(x, y2, 'r-', lw=5, alpha=0.6, label='George\nHill')
-plt.legend()
-
-
-
-
+if __name__ == "__main__":
+    set_plot_params(14)
+    df, a, b = organize_data()
+    make_plots(df, a, b)
 
 
