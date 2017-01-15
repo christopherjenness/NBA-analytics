f1d5cd6b146dd730f7ee54874b48e9ec3b832077
Christopher Jenness <christopherjenness@gmail.com>
2017-01-15 11:50:10 -0500

    Organize rebounding script

--- REB/reb.py
+++ REB/reb.py
@@ -1,23 +1,34 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.patches as mpatches
 
-df = pd.read_csv('2016reb.csv')
+def organize_data():
+    df = pd.read_csv('2016reb.csv')
+    df['OKC'] = df.TEAM == 'OKC'
+    OKC_players = df[df.OKC==True]
+    league_players = df[df.OKC==False]
 
-plt.scatter(df.CONTESTEDREB, df.UNCONTESTEDREB)
-
-df.sort_values('UNCONTESTEDREB', ascending=False)[['PLAYER', 'CONTESTEDREB', 'UNCONTESTEDREB']].head()
-
-df.sort_values('CONTESTEDREB', ascending=False)[['PLAYER', 'TEAM', 'CONTESTEDREB', 'UNCONTESTEDREB']].head(25)
-
-df['CHI'] = df.TEAM == 'CHI'
-df['OKC'] = df.TEAM == 'OKC'
-
-plt.scatter(df.CONTESTEDREB, df.UNCONTESTEDREB, c=df['OKC'])
-
-for team in df.TEAM.unique():
+def make_plots(df):
+    """
+    Plot of contested vs uncontested rebounds,
+    highlighting OKC
+    """
     plt.figure()
-    df[team] = df.TEAM == team
-    plt.scatter(df.CONTESTEDREB, df.UNCONTESTEDREB, c=df[team])
-    plt.title(team)
-    plt.show()
+    plt.scatter(OKC_players.CONTESTEDREB, OKC_players.UNCONTESTEDREB, c=sns.xkcd_palette(['windows blue']), label='OKC')
+    plt.scatter(league_players.CONTESTEDREB, league_players.UNCONTESTEDREB, c=sns.xkcd_palette(['amber']), label='League')
+    legend = plt.legend(frameon=True, loc=4)
+    frame = legend.get_frame()
+    frame.set_edgecolor('black')
+    plt.xlim(0, 250)
+    plt.ylim(0, 350)
+    plt.title('OKC Rebounding')
+    plt.xlabel('Contested Rebounds')
+    plt.ylabel('Uncontested Rebounds')
+    plt.text(90, 312, 'Westbrook')
+    plt.text(130, 125, 'Adams/Kanter')
+    plt.savefig('plots/OKC.png')
+
+if __name__ == '__main__':
+    df = organize_data()
+    make_plots(df)
