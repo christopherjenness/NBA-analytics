277c9a324cd469da8f261e7bb646a4e648109315
Christopher Jenness <christopherjenness@gmail.com>
2017-03-11 18:56:40 -0500

    Add more scratch working.  I'm going to sleep

--- analysis/drives/drives.py
+++ analysis/drives/drives.py
@@ -16,7 +16,7 @@ def set_plot_params(size):
     plt.rc('ytick', labelsize=SIZE)
     plt.rc('legend', fontsize=SIZE) 
     
-def make_mia_plots(df, team_df):
+def make_mia_plots(df, team):
     fig, axs = plt.subplots(1, 2, figsize=(18, 6))
     plt.axes(axs[0])
     team['PTSperDRIVE'] = team['PTS']/team['DRIVES']
@@ -38,11 +38,13 @@ def make_mia_plots(df, team_df):
 
 plt.scatter(team.DRIVES, team.PTS)
 
-df.sort('FGA')[['PLAYER', 'DRIVES']]
+df['Dragic'] = df.PLAYER == 'Goran Dragic'
+df.sort('PASS')[['PLAYER', 'PASS', 'DRIVES']]
 
-plt.scatter(df.DRIVES, df.TO)
+plt.scatter(df.DRIVES, df.FGM, c=df['Dragic'])
+plt.scatter(df.DRIVES, df.FTA, c=df['Dragic'])
 plt.scatter(df.DRIVES, df.PASS)
-plt.scatter(df.FGA, df.FTA)
+plt.scatter(df.PASS, df.AST, c=df['Dragic'])
 plt.scatter(df.FGA, df.AST)
 
 df.columns.values[2:]
@@ -56,25 +58,14 @@ plt.savefig('team')
 team['PTS']/team['DRIVES']
 
 df, team = load_data()
+make_mia_plots(df, team)
 
-plt.plot(x = team.index, y = (team['PTS']/team['DRIVES']))
+plt.scatter(df['DRIVES'], df['PASS'], c=df['Dragic'])
+plt.xlim(0, 900)
+plt.ylim(0, 350)
+plt.xlabel('Total Drives')
+plt.ylabel('Total passes out of drives')
+plt.title('Dragic likes to pass out of drives')
+plt.savefig('plots/dragicPASS')
 
-
-fig, axs = plt.subplots(1, 2, figsize=(18, 6))
-plt.axes(axs[0])
-team['PTSperDRIVE'] = team['PTS']/team['DRIVES']
-team = team.sort_values('PTSperDRIVE')
-sns.barplot(x =team.index.values, y=team['PTSperDRIVE'], color='seagreen')
-plt.xticks(rotation=90)
-plt.ylabel('PTS per drive')
-plt.title('Points per drive by team')
-
-plt.axes(axs[1])
-mia = df[df.TEAM == 'MIA']
-mia = mia.sort_values('DRIVES')
-sns.barplot(x = mia['PLAYER'], y=mia['DRIVES'], color='seagreen')
-plt.xticks(rotation=90)
-plt.xlabel('')
-plt.ylabel('Total Drives')
-plt.title('Total drives for MIA players')
-plt.savefig('plots/teams')
+sns.regplot(MIA['PASS'], MIA['AST'])
