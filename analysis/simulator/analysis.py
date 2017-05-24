510a64ea2bcb3fc340ba9ae83cc947d15f0bda91
Christopher Jenness <christopherjenness@gmail.com>
2017-05-24 05:37:11 -0400

    Add defensive code and plot

--- analysis/simulator/analysis.py
+++ analysis/simulator/analysis.py
@@ -6,6 +6,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
 def load_data(filename):
     df = pickle.load(open('scraper/data/{filename}'.format(filename=filename),
                           'rb'))
@@ -15,8 +16,10 @@ def load_data(filename):
     df.columns.values[4] = team2 + '-PTS'
     return df, team1, team2
 
+
 def add_results(df, team1, team2, timeout_dict):
-    timeouts = df[df[team1].str.contains('full timeout').fillna(False)].index.tolist()
+    timeouts = df[df[team1].str.contains('full timeout')
+                  .fillna(False)].index.tolist()
     for timeout in timeouts:
         outcome = False
         row = timeout
@@ -34,7 +37,8 @@ def add_results(df, team1, team2, timeout_dict):
             outcome = True
         timeout_dict[team1].append(outcome)
 
-    timeouts = df[df[team2].str.contains('full timeout').fillna(False)].index.tolist()
+    timeouts = df[df[team2].str.contains('full timeout')
+                  .fillna(False)].index.tolist()
     for timeout in timeouts:
         outcome = False
         row = timeout
@@ -54,11 +58,59 @@ def add_results(df, team1, team2, timeout_dict):
 
     return timeout_dict
 
-def analyze_all():
+
+def add_defensive_results(df, team1, team2, timeout_dict):
+    timeouts = df[df[team1].str.contains('full timeout')
+                  .fillna(False)].index.tolist()
+    for timeout in timeouts:
+        outcome = False
+        row = timeout
+        while (df.iloc[row].str.contains('enters').sum() > 0) or \
+              (df.iloc[row].str.contains('free throw').sum() > 0):
+            row += 1
+        if df.iloc[row][5]:
+            outcome = np.NaN
+            continue
+        while (not df.iloc[row][2]) and (not df.iloc[row][5]):
+            if int(row) == int(df.shape[0]) - 1:
+                break
+            row += 1
+        if df.iloc[row][2]:
+            outcome = True
+        timeout_dict[team2].append(outcome)
+
+    timeouts = df[df[team2].str.contains('full timeout')
+                  .fillna(False)].index.tolist()
+    for timeout in timeouts:
+        outcome = False
+        row = timeout
+        while (df.iloc[row].str.contains('enters').sum() > 0) or \
+              (df.iloc[row].str.contains('free throw').sum() > 0):
+            row += 1
+        if df.iloc[row][1]:
+            outcome = np.NaN
+            continue
+        while (not df.iloc[row][4]) and (not df.iloc[row][1]):
+            if int(row) == int(df.shape[0]) - 1:
+                break
+            row += 1
+        if df.iloc[row][4]:
+            outcome = True
+        timeout_dict[team1].append(outcome)
+
+    return timeout_dict
+
+
+def analyze_all(defense=False):
     timeout_dict = defaultdict(list)
     for fname in os.listdir('./scraper/data'):
         df, team1, team2 = load_data(fname)
-        timeout_dict = add_results(df, team1, team2, timeout_dict)
+        if defense:
+            timeout_dict = add_defensive_results(df, team1,
+                                                 team2, timeout_dict)
+        else:
+            timeout_dict = add_results(df, team1,
+                                       team2, timeout_dict)
     return timeout_dict
 
 
@@ -79,6 +131,25 @@ def make_plots():
     plt.tight_layout()
     plt.ylim(0.3, 0.5)
     plt.savefig('plots/teams')
+
+    defensive_results = analyze_all(defense=True)
+    conversion_dict = {}
+    for team in defensive_results.keys():
+        conversion_dict[team] = 1.0 - \
+                                (np.sum(defensive_results[team]) /
+                                 len(defensive_results[team]))
+
+    df_def = pd.DataFrame.from_dict(conversion_dict, orient='index')
+    df_def.columns = ['Conversion Percentage']
+    df_def = df_def.sort_values('Conversion Percentage')
+
+    sns.barplot(df_def.index, df_def['Conversion Percentage'],
+                color='seagreen')
+    plt.ylabel('Post-Timeout Defensive Stop Percentage')
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.ylim(0.45)
+    plt.savefig('plots/def')
 
 
 if __name__ == '__main__':
